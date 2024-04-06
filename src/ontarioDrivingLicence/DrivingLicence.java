d8a1d642057e224e259ef251f5c5cb06e4146f17
Gian Kaur <giankaur4@gmail.com>
2024-04-06 00:10:08 -0400

    Issue Ontario Driving Licence

--- src/ontarioDrivingLicence/DrivingLicence.java
+++ src/ontarioDrivingLicence/DrivingLicence.java
@@ -16,39 +16,43 @@ public class DrivingLicence {
 	boolean isClassGPass;
 	int noOfMonthsSinceG1Passed;
 	int noOfMonthsSinceG2Passed;
-	
-	/*declaring previous experience variable, 
-	if the immigrant having more than 2 year of experience from his back home 
-	can also give G test after passing G1. */
+
+	/*
+	 * declaring previous experience variable, if the immigrant having more than 2
+	 * year of experience from his back home can also give G test after passing G1.
+	 */
 
 	int previousExperience;
-	
-	
+
 	void isDrivingLicenceIssued() {
-		
+
 		System.out.println("Full Name: " + name + '\n' + "Age: " + age);
 
 		passingMarks = noOfQuestions - noOfWrongAnswers;
 		isWrittenExamPass = passingMarks >= 32;
 		isClassG1Pass = isVisionTestPass && isWrittenExamPass;
 		isClassG2Pass = isClassG1Pass && isRoadTestPass && noOfMonthsSinceG1Passed >= 8;
-		isClassGPass = (isClassG1Pass && age>=18 && previousExperience >=2) || (isClassG2Pass && isHighwayRoadTestPass && noOfMonthsSinceG2Passed >= 8);
+		isClassGPass = (isClassG1Pass && age >= 18 && previousExperience >= 2)
+				|| (isClassG2Pass && isHighwayRoadTestPass && noOfMonthsSinceG2Passed >= 8);
 
 		if (age >= 16) {
 
 			if (isClassG1Pass) {
-				
-                System.out.println("Vision test clear" + '\n' + "Number of Correct answers: " + passingMarks);
-                
-				System.out.println("Congratulations! You have passed the written exam." + '\n' + "Class G1 Licence has been issued.");
+
+				System.out.println("Vision test clear" + '\n' + "Number of Correct answers: " + passingMarks);
+
+				System.out.println("Congratulations! You have passed the written exam." + '\n'
+						+ "Class G1 Licence has been issued.");
 
 				if (isClassG2Pass) {
 
-					System.out.println("Congratulations! You have passed the road test." + '\n' + "Class G2 Licence has been issued.");
+					System.out.println("Congratulations! You have passed the road test." + '\n'
+							+ "Class G2 Licence has been issued.");
 
 					if (isClassGPass) {
 
-						System.out.println("Congratulations! You have passed the highway road test." + '\n' + "Class G Licence has been issued." + '\n' + "You are fully licenced now.");
+						System.out.println("Congratulations! You have passed the highway road test." + '\n'
+								+ "Class G Licence has been issued." + '\n' + "You are fully licenced now.");
 					} else {
 
 						System.out.println("Highway Road test failed.");
