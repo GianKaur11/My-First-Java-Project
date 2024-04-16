2ed77f23be7187e7dd3281eb61261f1917de2525
Gian Kaur <giankaur4@gmail.com>
2024-04-16 07:49:02 -0400

    Water Tank Update

--- src/waterTank/FillWaterTank.java
+++ src/waterTank/FillWaterTank.java
@@ -1,5 +1,7 @@
 package waterTank;
 
+import java.util.Scanner;
+
 /*There is a water tank of capacity 100 litres. 
  * Water is filled into it using bucket which have capacity of 10 litres. 
  * Write a program which fills the tank to its capacity using these buckets. 
@@ -9,23 +11,54 @@ package waterTank;
 public class FillWaterTank {
 
 	int noOfBuckets;
+	int capacityOfBucket;
+	int waterInTank;
+	int remainingCapacity;
+	String addMoreWater;
+
+	Scanner sc = new Scanner(System.in);
 
 	void checkIfWaterTankIsFull() {
 
-		for (int capacity = 0; capacity <= 100; capacity = capacity + 10) {
+		System.out.println("How much water is in the tank?");
+		waterInTank = sc.nextInt();
 
-			System.out.println("Tank has " + noOfBuckets + " buckets and " + capacity + " litres of water in it.");
+		for (int capacityOfTank = waterInTank; capacityOfTank <= 95; capacityOfTank = capacityOfTank
+				+ capacityOfBucket) {
 
-			noOfBuckets = noOfBuckets + 1;
-
-			if (capacity == 100) {
+			System.out.println("Tank has " + capacityOfTank + " litres of water in it.");
+			
+			if (capacityOfTank == 95) {
 
 				System.out.println("Tank is full. There is no more capacity in it.");
+				break;
+			
+			} else if (capacityOfTank < 95) {
+				System.out.println("Do you want to add more water?");
+				addMoreWater = sc.next();
+			}
+			
+			if (addMoreWater.equals("yes")) {
 
+				noOfBuckets = noOfBuckets + 1;
+				System.out.println("Enter capacity of bucket: ");
+				capacityOfBucket = sc.nextInt();
+
+				remainingCapacity = 95 - waterInTank;
+
+				if (capacityOfBucket > remainingCapacity) {
+
+					System.out.println("Enter less capacity to fill tank to max capacity.");
+					capacityOfBucket = 0;
+				}
+
+			} else {
+
+				System.out.println("Thank You! Have a nice day.");
+				break;
 			}
 
 		}
-
 	}
 
 }
